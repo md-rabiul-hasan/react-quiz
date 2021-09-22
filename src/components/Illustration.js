0e005a5671651d97dc6df8da7296cac391dd530f
rabiulhasan3 <rabiul.fci@gmail.com>
2021-09-23 01:22:00 +0600

    singup and login page design

---
+++ src/components/Illustration.js
@@ -0,0 +1,13 @@
+import React from "react";
+import image from "../assets/images/signup.svg";
+import classes from "../styles/Illustration.module.css";
+
+const Illustration = () => {
+  return (
+    <div className={classes.illustration}>
+      <img src={image} alt="Signup" />
+    </div>
+  );
+};
+
+export default Illustration;
---
+++ src/components/pages/Signup.js
@@ -0,0 +1,42 @@
+import React from "react";
+import classes from "../../styles/Signup.module.css";
+import Button from "../Button";
+import Checkbox from "../Checkbox";
+import Form from "../Form";
+import Illustration from "../Illustration";
+import TextInput from "../TextInput";
+
+const Signup = () => {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div className="column">
+        <Illustration />
+        <Form className={classes.singup}>
+          <TextInput icon="person" type="text" placeholder="Enter name" />
+          <TextInput
+            icon="alternate_email"
+            type="email"
+            placeholder="Enter email"
+          />
+          <TextInput icon="lock" type="password" placeholder="Enter password" />
+          <TextInput
+            icon="lock_clock"
+            type="password"
+            placeholder="Confirm password"
+          />
+
+          <Checkbox text="I agree to the Terms &amp; Conditions" />
+
+          <Button>Submit now</Button>
+
+          <div className="info">
+            Already have an account? <a href="login.html">Login</a> instead.
+          </div>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
